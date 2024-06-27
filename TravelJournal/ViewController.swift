dede16cbcc98f314d568e4552490513c9e5538e7
melikesoygullucu <melikesoygul@hotmail.com>
2024-06-27 09:39:48 +0300

    Initial Commit

---
+++ TravelJournal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TravelJournal
+//
+//  Created by Melike Soygüllücü on 27.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
