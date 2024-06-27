ef99cfb1cb816c7d83bf98f3f7580b428a9f13b4
melikesoygullucu <melikesoygul@hotmail.com>
2024-06-27 10:54:14 +0300

    mapkit and initializations added

--- TravelJournal/ViewController.swift
+++ TravelJournal/ViewController.swift
@@ -6,12 +6,22 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    var locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        mapView.delegate = self
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
     }
 
 
