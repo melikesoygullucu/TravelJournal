a672b8011bf4b5e970e982920540738b7ae65991
melikesoygullucu <melikesoygul@hotmail.com>
2024-06-29 16:26:33 +0300

    tableview added

---
+++ TravelJournal/ListViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ListViewController.swift
+//  TravelJournal
+//
+//  Created by Melike Soygüllücü on 29.06.2024.
+//
+
+import UIKit
+
+class ListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButton))
+
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    @objc func addButton() {
+        performSegue(withIdentifier: "toViewController", sender: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        var content = cell.defaultContentConfiguration()
+        content.text = "Hey"
+        cell.contentConfiguration = content
+        return cell
+    }
+}
